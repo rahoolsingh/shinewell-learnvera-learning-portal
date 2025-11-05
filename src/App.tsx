b88bb969b8a20b3c02c5f18c34c5d85a312f57eb
Veer Rajpoot <rahulksingh3907@gmail.com>
2025-11-05 17:28:35 +0530

    fix: Correct syntax errors and improve WhatsApp link implementation in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from "react-router/dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./animation.css"
+import "./animation.css";
 import CourseDetails from "./pages/CourseDetails";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     {
         path: "*",
         element: <Home />,
-    }
+    },
 ]);
 
 function App() {
@@ -48,13 +48,13 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <Header />
             {children}
 
-            <span className="" >
-                <img src={whatsAppIcon} alt="WhatsApp" className="w-12 h-12 fixed bottom-6 right-6 cursor-pointer z-50 drop-shadow-lg hover:scale-105 transition-transform duration-300"
-                    onClick={() => {
-                        window.open("https://wa.me/1234567890", "_blank");
-                    }}
+            <a className="" href="https://wa.me/+919262386604" target="_blank" rel="noopener noreferrer">
+                <img
+                    src={whatsAppIcon}
+                    alt="WhatsApp"
+                    className="w-12 h-12 fixed bottom-6 right-6 cursor-pointer z-50 drop-shadow-lg hover:scale-105 transition-transform duration-300"
                 />
-            </span>
+            </a>
 
             <Footer />
         </div>
